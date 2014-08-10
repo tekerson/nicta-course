4b1422354bcad8cd47cf8811a6a5180996624add
Brenton Alker <brenton@tekerson.com>
2014-08-10 15:30:04 +1000

    Complete Comonad module

--- src/Course/Comonad.hs
+++ src/Course/Comonad.hs
@@ -24,8 +24,7 @@ instance Comonad Id where
   copure ::
     Id a
     -> a
-  copure =
-    error "todo"
+  copure (Id a) = a
 
 -- | Witness that all things with (<<=) and copure also have (<$>).
 --
@@ -36,5 +35,5 @@ instance Comonad Id where
   (a -> b)
   -> f a
   -> f b
-(<$>) =
-  error "todo"
+f <$> fa = f . copure <<= fa
+
